21dea15742f2d67a6ab7da133cf22caa9bcfb330
toyz <toyzz@live.com>
2025-08-29 19:08:51 -0700

    build is failing?

--- examples/complete-app/internal/services/autogen_module.go
+++ examples/complete-app/internal/services/autogen_module.go
@@ -17,6 +17,26 @@ type CrawlerServiceInterface interface {
 	Stop(ctx context.Context) error
 }
 
+func NewCrawlerService() *CrawlerService {
+	return &CrawlerService{}
+}
+
+func initCrawlerServiceLifecycle(lc fx.Lifecycle, service *CrawlerService) {
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			go func() {
+				if err := service.Start(ctx); err != nil {
+					log.Printf("background start error in %s: %v", "CrawlerService", err)
+				}
+			}()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return service.Stop(ctx)
+		},
+	})
+}
+
 func NewDatabaseService(Config *config.Config) *DatabaseService {
 	return &DatabaseService{
 		Config: Config,
@@ -60,38 +80,18 @@ func initUserServiceLifecycle(lc fx.Lifecycle, service *UserService) {
 	})
 }
 
-func NewCrawlerService() *CrawlerService {
-	return &CrawlerService{}
-}
-
-func initCrawlerServiceLifecycle(lc fx.Lifecycle, service *CrawlerService) {
-	lc.Append(fx.Hook{
-		OnStart: func(ctx context.Context) error {
-			go func() {
-				if err := service.Start(ctx); err != nil {
-					log.Printf("background start error in %s: %v", "CrawlerService", err)
-				}
-			}()
-			return nil
-		},
-		OnStop: func(ctx context.Context) error {
-			return service.Stop(ctx)
-		},
-	})
-}
-
 func NewCrawlerServiceInterface(impl *CrawlerService) CrawlerServiceInterface {
 	return impl
 }
 
 // AutogenModule provides all core services in this package
 var AutogenModule = fx.Module("services",
+	fx.Provide(NewCrawlerService),
+	fx.Invoke(initCrawlerServiceLifecycle),
 	fx.Provide(NewDatabaseService),
 	fx.Invoke(initDatabaseServiceLifecycle),
 	fx.Provide(NewSessionServiceFactory),
 	fx.Provide(NewUserService),
 	fx.Invoke(initUserServiceLifecycle),
-	fx.Provide(NewCrawlerService),
-	fx.Invoke(initCrawlerServiceLifecycle),
 	fx.Provide(NewCrawlerServiceInterface),
 )
